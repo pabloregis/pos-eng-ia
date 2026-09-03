dfc9f9d600eefe320dab21263dd45d154843cfef
Pablo Regis <pablo.sregis@gmail.com>
2026-09-03 08:56:46 -0300

    feat: criar estrutura inicial de nodes e edges no LangGraph

--- modulo02/02-langchain-intro/src/graph/nodes/identifyIntentNode.ts
+++ modulo02/02-langchain-intro/src/graph/nodes/identifyIntentNode.ts
@@ -1,11 +1,11 @@
-import { type GraphState } from "../graph";
+import { type GraphState } from "../graph.ts";
 
 export function identifyIntent(state: GraphState): GraphState {
   const input = state.messages.at(-1)?.text ?? "";
   const inputLower = input.toLowerCase();
   let command: GraphState["command"] = "unknown";
 
-  if (inputLower.includes("uppercase")) {
+  if (inputLower.includes("upper")) {
     command = "uppercase";
   } else if (inputLower.includes("lower")) {
     command = "lowercase";
